e3613607ae6afe40ee32efb274c4e3121f9b1d95
Anokatabc <julian.voigt1@gmail.com>
2025-11-23 22:59:54 +0100

    Stand

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"net/http"
 	"strconv"
 	"strings"
@@ -16,6 +17,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+var debug bool = true
+
 //go:embed templates/*
 //go:embed static/*
 var embeddedFS embed.FS
@@ -29,6 +32,13 @@ func init() {
 }
 
 func main() {
+	if debug {
+		fs.WalkDir(embeddedFS, ".", func(path string, d fs.DirEntry, err error) error {
+			fmt.Println("Embedded file:", path)
+			return nil
+		})
+	}
+
 	router := httprouter.New()
 
 	defer func() {
@@ -36,50 +46,56 @@ func main() {
 	}()
 
 	//router.GET("/index.html", )
-	router.GET("/", func(w http.ResponseWriter, r *http.Request, params httprouter.Params){
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 
-	err := templates.ExecuteTemplate(w, indexHtml, nil)
+		err := templates.ExecuteTemplate(w, indexHtml, nil)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
-	router.POST("/", func(w http.ResponseWriter, r *http.Request, params httprouter.Params){
+	router.POST("/", func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 		err := r.ParseForm()
 		if err != nil {
 			fmt.Print(fmt.Errorf("Parse-Fehler! Beende Funktion"))
 			return
 		}
-		  leftOne := r.FormValue("leftOne")
-			rightOne := r.FormValue("rightOne")
-			leftTwo := r.FormValue("leftTwo")
-			rightTwo := r.FormValue("rightTwo")
-			leftThree := r.FormValue("leftThree")
-			rightThree := r.FormValue("rightThree")
-			leftFour := r.FormValue("leftFour")
-			rightFour := r.FormValue("rightFour")
-			leftFive := r.FormValue("leftFive")
-			rightFive := r.FormValue("rightFive")
-			timeResult, timeResultt, errTwo := calculateHours(leftOne, rightOne)
-			timeResultTwo, timeResulttTwo, errThree := calculateHours(leftTwo, rightTwo)
-			timeResultThree, timeResulttThree, errFour := calculateHours(leftThree, rightThree)
-			timeResultFour, timeResulttFour, errFive := calculateHours(leftFour, rightFour)
-			timeResultFive, timeResulttFive, errSix := calculateHours(leftFive, rightFive)
-			if (errTwo != nil)||(errThree != nil)||(errFour != nil)||(errFive != nil)||(errSix != nil) {
-				fmt.Print(fmt.Errorf("error calculateHours"))
-			}
+		leftOne := r.FormValue("leftOne")
+		rightOne := r.FormValue("rightOne")
+		leftTwo := r.FormValue("leftTwo")
+		rightTwo := r.FormValue("rightTwo")
+		leftThree := r.FormValue("leftThree")
+		rightThree := r.FormValue("rightThree")
+		leftFour := r.FormValue("leftFour")
+		rightFour := r.FormValue("rightFour")
+		leftFive := r.FormValue("leftFive")
+		rightFive := r.FormValue("rightFive")
+		timeResult, timeResultt, errTwo := calculateHours(leftOne, rightOne)
+		timeResultTwo, timeResulttTwo, errThree := calculateHours(leftTwo, rightTwo)
+		timeResultThree, timeResulttThree, errFour := calculateHours(leftThree, rightThree)
+		timeResultFour, timeResulttFour, errFive := calculateHours(leftFour, rightFour)
+		timeResultFive, timeResulttFive, errSix := calculateHours(leftFive, rightFive)
+		if (errTwo != nil) || (errThree != nil) || (errFour != nil) || (errFive != nil) || (errSix != nil) {
+			fmt.Print(fmt.Errorf("error calculateHours"))
+		}
 
-        // Ergebnis an Browser senden
-        fmt.Fprintf(w, "<h1><ul>Tag 1</ul></h1><p>%v Stunden, %v Minuten</p>", timeResult, timeResultt)
-        fmt.Fprintf(w, "<h1><ul>Tag 2</ul></h1><p>%v Stunden, %v Minuten</p>", timeResultTwo, timeResulttTwo)
-        fmt.Fprintf(w, "<h1><ul>Tag 3</ul></h1><p>%v Stunden, %v Minuten</p>", timeResultThree, timeResulttThree)
-        fmt.Fprintf(w, "<h1><ul>Tag 4</ul></h1><p>%v Stunden, %v Minuten</p>", timeResultFour, timeResulttFour)
-        fmt.Fprintf(w, "<h1><ul>Tag 5</ul></h1><p>%v Stunden, %v Minuten</p>", timeResultFive, timeResulttFive)
-        fmt.Fprintf(w, "<h1><ul>Gesamt:</ul></h1><p>%v Stunden, %v Minuten</p>", timeResult+timeResultTwo+timeResultThree+timeResultFour+timeResultFive, timeResultt+timeResulttTwo+timeResulttThree+timeResulttFour+timeResulttFive)
+		// Ergebnis an Browser senden
+		fmt.Fprintf(w, "<h1><ul>Tag 1</ul></h1><p>%v Stunden, %v Minuten</p>", timeResult, timeResultt)
+		fmt.Fprintf(w, "<h1><ul>Tag 2</ul></h1><p>%v Stunden, %v Minuten</p>", timeResultTwo, timeResulttTwo)
+		fmt.Fprintf(w, "<h1><ul>Tag 3</ul></h1><p>%v Stunden, %v Minuten</p>", timeResultThree, timeResulttThree)
+		fmt.Fprintf(w, "<h1><ul>Tag 4</ul></h1><p>%v Stunden, %v Minuten</p>", timeResultFour, timeResulttFour)
+		fmt.Fprintf(w, "<h1><ul>Tag 5</ul></h1><p>%v Stunden, %v Minuten</p>", timeResultFive, timeResulttFive)
+		fmt.Fprintf(w, "<h1><ul>Gesamt:</ul></h1><p>%v Stunden, %v Minuten</p>", timeResult+timeResultTwo+timeResultThree+timeResultFour+timeResultFive, timeResultt+timeResulttTwo+timeResulttThree+timeResulttFour+timeResulttFive)
 
 	})
 	router.GET("/health", metrics.Uptime())
 
-	router.ServeFiles("/static/*filepath", http.FS(embeddedFS))
+	//router.ServeFiles("/static/*filepath", http.FS(embeddedFS))
+	//router.ServeFiles("/static/*filepath", http.Dir("static"))
+	staticFS, errFS := fs.Sub(embeddedFS, "static")
+	if errFS != nil{
+		log.Fatal().Err(errFS).Msg("sub-filesystem error")
+	}
+	router.ServeFiles("/static/*filepath", http.FS(staticFS))
 
 	httpServer := http.Server{Addr: ":3000", Handler: router}
 	fmt.Println("Server läuft auf http://localhost:3000\n- abrufbar via Browser über diese Adresse")
@@ -94,31 +110,36 @@ func main() {
 func calculateHours(left, right string) (hours, minutes int, err error) {
 	leftHours, leftMinutes, err := parseTime(left)
 	if err != nil {
-		return 0,0, err
+		return 0, 0, err
 	}
 	rightHours, rightMinutes, err := parseTime(right)
 	if err != nil {
-		return 0,0, err
+		return 0, 0, err
 	}
 
 	startShift := leftHours*60 + leftMinutes
 	endShift := rightHours*60 + rightMinutes
-	
-	workminutes := (endShift-startShift)%60
-	workhours := int((endShift-startShift)/60)
 
+	workminutes := (endShift - startShift) % 60
+	workhours := int((endShift - startShift) / 60)
 
 	return workhours, workminutes, nil
 }
 
-func parseTime(timeStr string) (hours, minutes int, err error){
-  timeArr := strings.Split(timeStr, ":")
-  if len(timeArr) != 2 {return 0, 0, err}
+func parseTime(timeStr string) (hours, minutes int, err error) {
+	timeArr := strings.Split(timeStr, ":")
+	if len(timeArr) != 2 {
+		return 0, 0, err
+	}
 
 	hours, err = strconv.Atoi(timeArr[0])
-	if err != nil {return 0, 0, fmt.Errorf("")}
+	if err != nil {
+		return 0, 0, fmt.Errorf("")
+	}
 	minutes, err = strconv.Atoi(timeArr[1])
-	if err != nil {return 0, 0, fmt.Errorf("")}
+	if err != nil {
+		return 0, 0, fmt.Errorf("")
+	}
 
 	if hours < 0 || hours > 23 {
 		return 0, 0, fmt.Errorf("Stunden müssen zwischen 0 und 23 liegen")
